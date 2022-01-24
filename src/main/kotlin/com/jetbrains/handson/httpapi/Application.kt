811a5523f61d500eaa615123daf31526547f134b
vakhaniya <vakhaniya.ga@gmail.com>
2022-01-24 20:47:46 +0300

    created customer route

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,16 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import com.jetbrains.handson.httpapi.routes.registerCustomRoutes
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.serialization.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    install(ContentNegotiation) {
+        json()
+    }
+    registerCustomRoutes()
 }
